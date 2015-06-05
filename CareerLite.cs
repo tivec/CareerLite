a392f4a87b44a0e97d5b2f0010a79db670355eba
Henrik Bergvin <henrik@bergvin.com>
2015-06-05 18:58:09 +0200

    Basic funcionality is in place for locking funds.

--- CareerLite.cs
+++ CareerLite.cs
@@ -12,33 +12,48 @@ namespace CareerLite
 
 		public CareerLite ()
 		{
-
+			Debug.Log("[CareerLite [" + this.GetInstanceID ().ToString ("X") + "][" + Time.time.ToString ("0.0000") + "]: Constructor");
 		}
 
 		public void FundsChanged(double amount, TransactionReasons reason)
 		{
-			ScreenMessages.PostScreenMessage ("CareerLite: Funds changed: " + amount);
+			// TODO: Add GUI so this can be toggled!
+			// if (!lockFunds)
+			//	return;
+
+			if (reason != TransactionReasons.Cheating)
+				Funding.Instance.AddFunds (double.MaxValue-amount, TransactionReasons.Cheating);
 
 		}
 
 
 		public void Start()
 		{
+			Debug.Log("[CareerLite [" + this.GetInstanceID ().ToString ("X") + "][" + Time.time.ToString ("0.0000") + "]: Start");
 			GameEvents.OnFundsChanged.Add (FundsChanged);
 
 		}
 
 		public override void OnAwake ()
 		{
+
 		}
 
 		public override void OnSave (ConfigNode node)
 		{
+			node.AddValue ("LockedFunds", lockFunds.ToString());
 		}
+
+		// TODO: Move this to AstrotechUtilities
+		private bool GetBool(ConfigNode node, string key) {
+			return bool.Parse (node.GetValue (key));
+		}
+
 
 		public override void OnLoad (ConfigNode node)
 		{
-
+			Debug.Log("[CareerLite [" + this.GetInstanceID ().ToString ("X") + "][" + Time.time.ToString ("0.0000") + "]: OnLoad.");
+			lockFunds = GetBool (node, "LockedFunds");
 		}
 
 		void OnDestroy()
